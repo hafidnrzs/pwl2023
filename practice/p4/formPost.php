42288cfd91c5aaa797788a03c31d8e2658b56b11
hafidnrzs <hafidnurazis@gmail.com>
2023-04-08 16:38:57 +0700

    Add Boostrap to POST data handling

--- practice/p4/formPost.php
+++ practice/p4/formPost.php
@@ -5,35 +5,74 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pengolahan Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-<h2>Registrasi</h2>
-<form action="prosesPost.php" method="post">
-Nama Depan: <input type="text" name="namaDpn"><br>
-Nama Belakang: <input type="text" name="namaBlk"><br>
-Email: <input type="text" name="email"><br>
-Jenis Member:
-    <input type="checkbox" name="cPengHobby" value="pengHobby" checked>pengHobby
-    <input type="checkbox" name="cPetani" value="petani">Petani
-    <input type="checkbox" name="cPedagang" value="pedagang">Pedagang
-    <input type="checkbox" name="cSales" value="sales">Sales
-    <input type="checkbox" name="cPengamat" value="pengamat">Pengamat<br>
-Pendidikan:
-    <select name="cbPendidikan">
-        <option value="TdkLulusSD">TdkLulusSD</option>
-        <option value="SD">SD</option>
-        <option value="SMP">SMP</option>
-        <option value="SMA">SMA</option>
-        <option value="Diploma">Diploma</option>
-        <option value="Sarjana">Sarjana</option>
-        <option value="Magister">Magister</option>
-        <option value="Doktor">Doktor</option>
-    </select><br>
-Gender:<br>
-    <input type="radio" name="rGender" value="pria" checked>Pria<br>
-    <input type="radio" name="rGender" value="wanita">Wanita<br>
-Masukan terhadap kami: <textarea name="saran" cols="40" row="5"></textarea><br>
-    <input type="submit">
-</form>
+    <main class="container">
+        <h2>Registrasi</h2>
+        <form action="prosesPost.php" method="post">
+            <div class="mb-3">
+                <label class="form-label" for="namaDpn">Nama Depan: </label>
+                <input class="form-control" type="text" name="namaDpn" id="namaDpn">
+            </div>
+            <div class="mb-3">
+                <label for="namaBlk" class="form-label">Nama Belakang:</label>
+                <input class="form-control" type="text" name="namaBlk">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input class="form-control" type="text" name="email">
+            </div>
+            Jenis Member:
+            <div class="form-check mb-3">
+                <div class="form-check">
+                    <input type="checkbox" name="cPengHobby" id="cPengHobby" value="pengHobby" checked class="form-check-input">
+                    <label class="form-check-label" for="cPengHobby">pengHobby</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" name="cPetani" id="cPetani" value="petani" class="form-check-input">
+                    <label class="form-check-label" for="cPetani">Petani</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" name="cPedagang" id="cPedagang" value="pedagang" class="form-check-input">
+                    <label class="form-check-label" for="cPedagang">Pedagang</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" name="cSales" id="cSales" value="sales" class="form-check-input">
+                    <label class="form-check-label" for="cSales">Sales</label>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" name="cPengamat" id="cPengamat" value="pengamat" class="form-check-input">
+                    <label class="form-check-label" for="cPengamat">pengamat</label>
+                </div>
+            </div>
+            <label for="cbPendidikan">Pendidikan:</label>
+            <select class="form-select" name="cbPendidikan" id="cbPendidikan">
+                <option value="TdkLulusSD">TdkLulusSD</option>
+                <option value="SD">SD</option>
+                <option value="SMA">SMP</option>
+                <option value="SMP">SMA</option>
+                <option value="Diploma">Diploma</option>
+                <option value="Sarjana">Sarjana</option>
+                <option value="Magister">Magister</option>
+                <option value="Doktor">Doktor</option>
+            </select><br>
+            Gender:<br>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="rGender" id="pria" value="pria" checked>Pria
+                    <label class="form-check-label" for="pria">Pria</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="rGender" id="wanita" value="wanita">
+                    <label class="form-check-label" for="wanita">Wanita</label>
+                </div>
+                <div class="mb-3">
+                    <label for="saran" class="form-label">Masukan terhadap kami:</label>
+                    <textarea class="form-control" id="saran" name="saran" cols="40" row="5"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary">
+        </form>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
--- practice/p4/prosesPost.php
+++ practice/p4/prosesPost.php
@@ -1,44 +1,72 @@
-<?php
-    //proses textBox
-    $namaDpn = $_POST["namaDpn"];
-    $namaBlk = $_POST["namaBlk"];
-    $email = $_POST["email"];
-    echo "Nama anggota: $namaDpn $namaBlk <br>";
-    echo "Email: $email <br>";
+<!DOCTYPE html>
+<html lang="en">
 
-    //proses checkBox
-    $jenis = "";
-    if (isset($_POST["cPengHobby"])) {
-        $pengHobby = $_POST['cPengHobby'];
-        $jenis .= $pengHobby . " ";
-    }
-    if (isset($_POST["cPetani"])) {
-        $petani = $_POST['cPetani'];
-        $jenis .= $petani . " ";
-    }
-    if (isset($_POST["cPedagang"])) {
-        $pedagang = $_POST['cPedagang'];
-        $jenis .= $pedagang . " ";
-    }
-    if (isset($_POST["cSales"])) {
-        $sales = $_POST['cSales'];
-        $jenis .= $sales . " ";
-    }
-    if (isset($_POST["cPengamat"])) {
-        $pengamat = $_POST['cPengamat'];
-        $jenis .= $pengamat . " ";
-    }
-    $jenis = trim($jenis);
-    echo "Jenis member: $jenis <br>";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proses</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
 
-    //proses comboBox
-    $pendidikan = $_POST["cbPendidikan"];
-    echo "Pendidikan: $pendidikan<br>";
+<body>
+    <main class="container mt-4">
+        <div class="card text-center">
+            <div class="card-header text-body-secondary">
+                Member Baru
+            </div>
+            <?php
+            //proses textBox
+            $namaDpn = $_POST["namaDpn"];
+            $namaBlk = $_POST["namaBlk"];
+            $email = $_POST["email"];
+            echo "<div class='card-body'>";
+            echo "<h2 class='card-title'>$namaDpn $namaBlk</h2>";
+            echo "<p class='card-text'>$email</p>";
 
-    //proses radio
-    $gender = $_POST["rGender"];
-    //nl2br: untuk menyisipkan <br> pada string yang terdapat \n
-    echo nl2br("Gender: $gender \n");
-    $saran = nl2br($_POST["saran"]);
-    echo "Saran: $saran";
-?>
+            //proses comboBox
+            $pendidikan = $_POST["cbPendidikan"];
+            echo "<p class='card-text'>$pendidikan</p>";
+
+            //proses radio
+            $gender = $_POST["rGender"];
+            echo "<p class='card-text'>$gender</p>";
+            echo "<p class='card-text'>Masukan:</p>";
+            //nl2br: untuk menyisipkan <br> pada string yang terdapat \n
+            $saran = nl2br($_POST["saran"]);
+            echo "<p class='card-text'>$saran</p>";
+            echo "</div>";
+
+            echo "<div class='card-footer text-body-secondary'>";
+            //proses checkBox
+            $jenis = "";
+            if (isset($_POST["cPengHobby"])) {
+                $pengHobby = $_POST['cPengHobby'];
+                $jenis .= $pengHobby . " ";
+            }
+            if (isset($_POST["cPetani"])) {
+                $petani = $_POST['cPetani'];
+                $jenis .= $petani . " ";
+            }
+            if (isset($_POST["cPedagang"])) {
+                $pedagang = $_POST['cPedagang'];
+                $jenis .= $pedagang . " ";
+            }
+            if (isset($_POST["cSales"])) {
+                $sales = $_POST['cSales'];
+                $jenis .= $sales . " ";
+            }
+            if (isset($_POST["cPengamat"])) {
+                $pengamat = $_POST['cPengamat'];
+                $jenis .= $pengamat . " ";
+            }
+            $jenis = trim($jenis);
+            echo $jenis;
+            echo "</div>";
+            ?>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
