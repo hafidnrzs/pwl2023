90922fa8f403b3bdbf4a1195ed3cf58650c470b3
hafidnrzs <hafidnurazis@gmail.com>
2023-04-08 07:32:49 +0700

    String and math PHP function

---
+++ practice/p3/math.php
@@ -0,0 +1,13 @@
+<?php
+    echo (abs(-6.7)); // return 6.7 (nilai absolut)
+    echo "<br>";
+    echo (sqrt(64)); // output 8
+    echo "<br>";
+    echo (round(0.60)); // return 1, rounded up
+    echo "<br>";
+    echo (round(0.49)); // return 0, rounded down
+    echo "<br>";
+    echo (rand());
+    echo "<br>";
+    echo (rand(10, 100));
+?>
---
+++ practice/p3/string.php
@@ -0,0 +1,11 @@
+<?php
+    echo strlen("Hello world!"); // output 12
+    echo "<br>";
+    echo str_word_count("Hello world!"); // output 2
+    echo "<br>";
+    echo strrev("Hello world!"); // output !dlrow olleH
+    echo "<br>";
+    echo strpos("Hello world!", "world"); // output 6
+    echo "<br>";
+    echo str_replace("world", "Dolly", "Hello world!"); // output Hello Dolly
+?>
