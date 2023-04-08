bfa2b1256bf5575c612fb95082683bc73ee14e36
hafidnrzs <hafidnurazis@gmail.com>
2023-04-08 15:27:42 +0700

    PHP Cookies

---
+++ practice/p4/cookies/halaman1.php
@@ -0,0 +1,6 @@
+<?php
+    $user = $_COOKIE["username"];
+    echo "<h1>Halaman 1</h1>";
+    echo "<div>user: $user</div>";
+    echo "<div><a href='halaman2.php'>Link ke halaman 2</a></div>";
+?>
---
+++ practice/p4/cookies/halaman2.php
@@ -0,0 +1,6 @@
+<?php
+    $user = $_COOKIE["username"];
+    echo "<h1>Halaman 2</h1>";
+    echo "<div>user: $user</div>";
+    echo "<div><a href='login.php'>Link ke halaman login</a></div>";
+?>
